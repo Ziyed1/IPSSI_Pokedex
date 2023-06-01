c11978f99c32a8937702de7e111978c1f845116e
RestumpY <yannis.lanista@hotmail.com>
2023-06-01 14:41:51 +0200

    Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import App from './App.jsx'
-import './index.css'
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from './compenents/Navbar.jsx';
+
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <App />  
+    <Navbar/>
   </BrowserRouter>
 );
