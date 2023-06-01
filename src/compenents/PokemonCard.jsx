c61799006235cbcda52644c4a8cba6885c6b96f6
RestumpY <yannis.lanista@hotmail.com>
2023-06-01 15:35:29 +0200

    Alerte pour wishlist

--- src/compenents/PokemonCard.jsx
+++ src/compenents/PokemonCard.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -7,41 +7,57 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
-
-
-
-
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import Alert from '@mui/material/Alert';
 
 export default function PokemonCard({ id, image, name }) {
   const theme = useTheme();
+  const [showAlert, setShowAlert] = useState(false);
+
+  const formattedName = name.charAt(0).toUpperCase() + name.slice(1);
+
+  const handleHeartClick = () => {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 5000); // Disparaît après 5 secondes (5000 ms)
+  };
 
   return (
     <Card sx={{ display: 'flex' }}>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
         <CardContent sx={{ flex: '1 0 auto' }}>
           <Typography component="div" variant="h5">
-            {name}
+            {formattedName}
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" component="div">
             #{id}
           </Typography>
         </CardContent>
         <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-
-          <IconButton aria-label="play/pause">
-            <CatchingPokemonIcon
-              sx={{ height: 38, width: 38 }}
-            />
+          <IconButton>
+            <CatchingPokemonIcon sx={{ height: 20, width: 20 }} />
           </IconButton>
-
+          <IconButton onClick={handleHeartClick}>
+            <FavoriteBorderIcon sx={{ height: 20, width: 20 }} />
+          </IconButton>
         </Box>
       </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image={image}
-      />
-
+      <CardMedia component="img" sx={{ width: 151 }} image={image} />
+      {showAlert && (
+        <Alert
+          severity="success"
+          sx={{
+            position: 'fixed',
+            right: 16,
+            bottom: 16,
+            zIndex: 9999,
+          }}
+          onClose={() => setShowAlert(false)}
+        >
+          Ajouté dans votre wishlist
+        </Alert>
+      )}
     </Card>
   );
 }
