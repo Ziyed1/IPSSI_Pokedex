697120029ec31a3aea7aff3226f168cd9d922f5e
Florian R <47738387+FlorianROUVIER@users.noreply.github.com>
2023-06-02 15:17:54 +0200

    Add pokemon details

--- src/compenents/PokemonDetails.jsx
+++ src/compenents/PokemonDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from "react-query";
+import '../styles/PokemonDetails.css'
 
 
 export default function PokemonDetails() {
@@ -18,7 +19,26 @@ export default function PokemonDetails() {
       if (error) {
         return error.message;
       }
+
+      const types = data.types.map((typeData) => {
+        const typeName = typeData.type.name;
+        return typeName.charAt(0).toUpperCase() + typeName.slice(1);
+      });
+  
+      const formattedName = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+
   return (
-    <div> {data.name} </div>
+    <div className="pokemon_container"> 
+      <div className="pokemon_main_box">
+        <div className="pokemon_box">
+          <div className="pokemon_data data_name">
+                <h3>#{data.id} {formattedName}</h3>
+                <img src={data.sprites.other.home.front_default} alt={data.name} />
+                <span class="pokemon_data">  Type : {types}</span>
+                <span class="pokemon_data">   Taille {data.height}m / Poids : {data.weight}kg</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
