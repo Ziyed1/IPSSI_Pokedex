10ffbf11730272a16e372fe116aa0891da55df62
Florian R <dreathxtreme@gmail.com>
2023-06-04 17:23:51 +0200

    Add Shiny sprite

--- src/compenents/PokemonDetails.jsx
+++ src/compenents/PokemonDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/PokemonDetails.css';
 import { useParams } from 'react-router-dom'
 import { useQuery } from "react-query"
@@ -23,18 +23,32 @@ const typeClassMapping = {
 
 export default function PokemonDetails() {
   const { slug } = useParams();
+  const [showShinySprite, setShowShinySprite] = useState(false);
 
-  const { isLoading, error, data } = useQuery("pokemon", () =>
-    fetch("https://pokeapi.co/api/v2/pokemon/" + slug).then((res) =>
+  // Fetch API 
+
+  const { isLoading, error, data } = useQuery('pokemon', async () =>
+  await fetch('https://pokeapi.co/api/v2/pokemon/' + slug).then((res) =>
+    res.json()
+  )
+  
+);
+
+  const {
+    isLoading: speciesLoading,
+    error: speciesError,
+    data: speciesData,
+  } = useQuery('species', async() =>
+    await fetch('https://pokeapi.co/api/v2/pokemon-species/' + slug).then((res) =>
       res.json()
     )
   );
 
-  if (isLoading) {
-    return "Loading...";
+  if (isLoading || speciesLoading) {
+    return 'Loading...';
   }
-  if (error) {
-    return error.message;
+  if (error || speciesError) {
+    return error.message || speciesError.message;
   }
 
   const types = data.types.map((typeData) => {
@@ -46,21 +60,35 @@ export default function PokemonDetails() {
   const typeClassName = data.types[0].type.name; // Utilise le premier type pour déterminer la classe CSS
   const pokemonClass = typeClassMapping[typeClassName] || 'card-default';
 
+  let spriteUrl = data.sprites.other.home.front_default;
+  if (showShinySprite) {
+    spriteUrl = data.sprites.other.home.front_shiny;
+  }
+
+  const toggleShinySprite = () => {
+    setShowShinySprite((prevShowShinySprite) => !prevShowShinySprite);
+  };
+
   return (
     <div className="pokemon-details-container">
       <div className={`pokemon-details ${pokemonClass}`}>
         <div className="pokemon-info">
           <div className="pokemon-header">
-            <h1 className="pokemon-id">#{data.id} {formattedName}</h1>
+            <h1 className="pokemon-id">#{data.id} - {formattedName}</h1>
+            <h2> {speciesData.genera[7].genus}</h2>
+            <h3> </h3>
           </div>
           <div className="pokemon-image">
-            <img src={data.sprites.other.home.front_default} alt={formattedName} />
+            <img src={spriteUrl} alt={formattedName} />
+            <button className="shiny-toggle" onClick={toggleShinySprite}>
+              {showShinySprite ? 'Normal' : 'Chromatique'}
+            </button>
           </div>
         </div>
         <div className="pokemon-details-section">
           <div className="pokemon-types">
             <h3>Types:</h3>
-            <ul>
+            <ul className="types" >
               {types.map((type) => (
                 <li key={type}>{type}</li>
               ))}
